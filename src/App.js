4bf1f9bc33cccc2d4a4bc7703a6fc1b1b5330d86
Nico <nicorabarisoa@gmail.com>
2022-07-11 11:44:08 +0200

    single object const course

--- src/App.js
+++ src/App.js
@@ -29,28 +29,30 @@ const App = () => {
   
 
  
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course}/>
-      <Content part={parts[0].name} exercise={parts[0].exercises}/>
-      <Content part={parts[1].name} exercise={parts[1].exercises}/>
-      <Content part={parts[2].name} exercise={parts[2].exercises}/>
-      <Total calc={parts[0].exercises+parts[1].exercises+parts[2].exercises}/>
+      <Header course={course.name}/>
+      <Content part={course.parts[0].name} exercise={course.parts[0].exercises}/>
+      <Content part={course.parts[1].name} exercise={course.parts[1].exercises}/>
+      <Content part={course.parts[2].name} exercise={course.parts[2].exercises}/>
+      <Total calc={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>
     </div>
   )
 }
